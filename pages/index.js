eb3e7bf4062130769e80755f0eecc1bb70e17d17
Nimesh Vellera <nimeshvellera@gmail.com>
2022-10-03 10:31:27 +0530

    More updates

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube, AiFillCloud } from 'react-icons/ai';
 
 export default function Home() {
   return (
@@ -9,10 +11,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-      <section className='bg-blue-700'>
-        <h1>Hello World</h1>
-        </section>
+      <main className="bg-white px-10">
+      <section className='min-h-screen'>
+        <nav className="py-10 mb-12 flex justify-between">
+          <h1 className="text-xl font-burtons">developed by Nimesh</h1>
+          <ul className="flex items-center">
+            <li><BsFillMoonStarsFill className="cursor-pointer text-2xl"/></li>
+            <li>
+              <a className="bg bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
+              </li>
+          </ul>
+        </nav>
+
+        <div className="text-center p-10 ">
+          <h2 className="text-5xl py-2 text-teal-600 font-medium">Nimesh Vellera</h2>
+          <h3 className="text-2xl py-2">Data Scientist</h3>
+          <p className="text-md py-5 leading-8 text-gray-800">Data Scientist that doesn't stop at data science. Cloud Native. </p>
+        </div>
+        <div className="text-5xl flex">
+          <AiFillTwitterCircle/>
+          <AiFillLinkedin/>
+          <AiFillYoutube/>
+        </div>
+      </section>
       </main>
 
     </div>
